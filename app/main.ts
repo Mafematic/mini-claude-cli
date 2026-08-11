3cfacda49779eb6c75455a90342f39bf5ddb74c9
Friedrich Szendzielorz <friedrich.szendzielorz@nexenio.com>
2026-08-11 06:35:30 +0200

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -1,4 +1,5 @@
 import OpenAI from "openai";
+import * as fs from 'fs';
 
 async function main() {
   const [, , flag, prompt] = process.argv;
@@ -44,10 +45,21 @@ async function main() {
     throw new Error("no choices in response");
   }
 
-  // You can use print statements as follows for debugging, they'll be visible when running tests.
-  console.error("Logs from your program will appear here!");
 
-  console.log(response.choices[0].message.content);
+  // You can use print statements as follows for debugging, they'll be visible when running tests.
+  //console.error("Logs from your program will appear here!");
+  if (
+    !response.choices[0].message.tool_calls ||
+    response.choices[0].message.tool_calls.length === 0
+  ) {
+    console.log(response.choices[0].message.content);
+    throw new Error("no tool calls array");
+  }
+  console.log(response.choices[0].message.tool_calls[0].function.name);
+  let args = JSON.parse(response.choices[0].message.tool_calls[0].function.arguments);
+  const fileName = args.file_path;
+  const contents = fs.readFileSync(fileName,'utf8');
+  console.log(contents);
 }
 
 main();
